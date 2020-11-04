9bd792cb0127c8f8894b2edc409fea92884b7595
charles Eddy <16qeddy@gmail.com>
2020-11-04 14:21:34 -0800

    completed boilerplate for bable, webpack and react

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,2 +1,24 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
+//http://localhost:3000/events?q=Greece&_page=2&_limit=2
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      search: '',
+      query: '',
+      page: 1
+    }
+  }
+
+  render() {
+    return (
+      <div>hollo world</div>
+    );
+  }
+}
+
+export default App;
 
---
+++ challenge_1/webpack.config.js
@@ -0,0 +1,17 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  module: {
+
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
